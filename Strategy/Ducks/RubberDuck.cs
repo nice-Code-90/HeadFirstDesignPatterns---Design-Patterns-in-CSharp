3d1b06457328f3b2d04e3636a186158ac352a35b
Milan Mazik <mazik1990@hotmail.com>
2025-02-20 17:54:52 +0100

    rubberDuck added

---
+++ Strategy/Ducks/RubberDuck.cs
@@ -0,0 +1,24 @@
+﻿using Strategy.Behaviors.FlyBehaviors;
+using Strategy.Behaviors.QuackBehaviors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy.Ducks
+{
+    public class RubberDuck : Duck
+    {
+        public RubberDuck()
+        {
+            quackBehavior = new Squeak();
+            flyBehavior = new FlyNoWay();
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine("I'm a rubber duck");
+        }
+    }
+}
